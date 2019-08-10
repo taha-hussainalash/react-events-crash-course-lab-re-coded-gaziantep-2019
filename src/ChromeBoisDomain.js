9168e73b0dc11ac520e4aaede5af301f31d3d415
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-10 21:01:42 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -25,7 +25,7 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
-  
+   
   render() {
     return (
       <canvas
